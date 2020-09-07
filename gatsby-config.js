7d297c1ef28314129da9c4b32d3224c04fad8421
Richard Emate <richardemate@gmail.com>
2020-09-07 15:55:01 +0100

    added tips for the day

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,10 +31,23 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: process.env.FACEBOOK_PIXEL_ID,
-      },
+      resolve: `gatsby-plugin-facebook-multi-pixels`,
+      options: [
+        {
+          dev: false,
+          alias: 'pucpredictMakeDo',
+          pixelId: process.env.FACEBOOK_PIXEL_ID_PP,
+          viewContent: true,
+          pageView: true,
+        },
+        {
+          dev: false,
+          alias: 'betSmart',
+          pixelId: process.env.FACEBOOK_PIXEL_ID_BS,
+          viewContent: true,
+          pageView: true,
+        }
+      ]
     }
   ],
 }
